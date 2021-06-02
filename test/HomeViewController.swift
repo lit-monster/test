8ff802073b998ce230af6e09c1715cc872dd0740
Youmi Nagase <youmi.ngs@gmail.com>
2021-06-02 20:21:41 +0900

    表示ちょっとだけ、できるようにした。

--- test/HomeViewController.swift
+++ test/HomeViewController.swift
@@ -5,11 +5,26 @@
 //  Created by Youmi Nagase on 2021/01/06.
 //
 
+import CoreData
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, NSFetchedResultsControllerDelegate {
+
+    let dataManager = DataManager.shared
     
-    @IBOutlet var TableView :UITableView!
+    @IBOutlet var tableView :UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Plus> = {
+
+        let _controller: NSFetchedResultsController<Plus> = dataManager.getFetchedResultController(with: ["date"])
+        _controller.delegate = self
+        return _controller
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,17 +32,55 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-    /*
-    // MARK: - Navigation
+        do {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            try fetchedResultsController.performFetch()
+        } catch {
+
+            print(error)
+        }
     }
-    */
+}
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = fetchedResultsController.sections else { return 0 }
+
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects // これ
+        
+//        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let plus = fetchedResultsController.object(at: indexPath)
+        print(fetchedResultsController.object(at: indexPath))
+        print(plus.date)
+        print(plus.japanese)
+        print(plus.math)
+        print(plus.english)
+        print(plus.science)
+        print(plus.social_studies)
+        print(plus.ranking)
+        print(plus.name)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = String(plus.japanese)
+//        DateUtils.stringFromDate(date: plus.date!, format: "yyyy/MM/dd")
+        return cell!
+    }
+    
+    
+}
+
+class DateUtils {
+    class func stringFromDate(date: Date, format: String) -> String {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.dateFormat = format
+        return formatter.string(from: date)
+    }
 }
